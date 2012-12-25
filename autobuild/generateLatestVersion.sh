6ca29a3b2c27deb1df06a0347810180c5037b66d
daniw <dani.winz@bluewin.ch>
2012-12-25 01:54:28 +0100

    Versionen mit Taschenrechner in das Build-Script integriert.

--- autobuild/generateLatestVersion.sh
+++ autobuild/generateLatestVersion.sh
@@ -15,6 +15,12 @@ else
  hash=$(git log | head -n1 | sed -e 's/ /\n/g' | tail -n1)
  cp fosa_main.pdf $currpwd/build/fosa_$hash.pdf
  cp fosa_main.pdf $currpwd/build/fosa_latest.pdf
+ cp fosa_main_ti89.pdf $currpwd/build/fosa_$hash.pdf
+ cp fosa_main_ti89.pdf $currpwd/build/fosa_latest.pdf
+ cp fosa_main_tinspire.pdf $currpwd/build/fosa_$hash.pdf
+ cp fosa_main_tinspire.pdf $currpwd/build/fosa_latest.pdf
+ cp fosa_main_ti_both.pdf $currpwd/build/fosa_$hash.pdf
+ cp fosa_main_ti_both.pdf $currpwd/build/fosa_latest.pdf
  cd $currpwd
  echo $hash > latesthash
 fi
