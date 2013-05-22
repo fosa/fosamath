2bfd9af1eddebbc3e4ae9758fd3f09eb647fc4ac
Nino Ninux <nino.ninux@gmail.com>
2013-05-22 18:25:59 +0200

    Grafikausgabe von EPS auf SVG geändert für octave-script

--- field.m
+++ field.m
@@ -9,14 +9,14 @@ dgl = @(y, x) 3+2*y
 # Plot des Richtungsfeldes erstellen
 Richtungsfeld(dgl)
 
-print('field.eps', '-deps')
+print('field.svg', '-dsvg')
 
 # Orthogonale Trajektorien
 dgl1 = @(y, x) y
 Richtungsfeld(dgl1)
-print('trajektorien1.eps', '-deps')
+print('trajektorien1.svg', '-dsvg')
 
 dgl2 = @(y, x) -1/y
 Richtungsfeld(dgl2)
-print('trajektorien2.eps', '-deps')
+print('trajektorien2.svg', '-dsvg')
 
